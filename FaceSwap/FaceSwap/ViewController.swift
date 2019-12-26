6e485bf5a94ec45279c121a8a879fcd016c11b51
Alexander Karlsson <st.alexander85@gmail.com>
2019-12-26 17:44:56 +0100

    Remove unnecessary paths plot.

--- FaceSwap/FaceSwap/ViewController.swift
+++ FaceSwap/FaceSwap/ViewController.swift
@@ -51,43 +51,43 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
      Listens for swap button press. Is enabled if both images are not nil.
      */
     @IBAction func onSwapPressed(_ sender: Any) {
-//        let im_utils = ImageUtilsWrapper()
-//        let lmarks1: NSMutableArray = [0];
-//        let lmarks2: NSMutableArray = [0];
-//        let im3 = im_utils.swap(im1, face2: im2, landmarks1: lmarks1, landmarks2: lmarks2);
-
-//        let im3 = detectFaces(image: im1)
-//        let im4 = detectFaces(image: im2)
+        //        let im_utils = ImageUtilsWrapper()
+        //        let lmarks1: NSMutableArray = [0];
+        //        let lmarks2: NSMutableArray = [0];
+        //        let im3 = im_utils.swap(im1, face2: im2, landmarks1: lmarks1, landmarks2: lmarks2);
+        
+        //        let im3 = detectFaces(image: im1)
+        //        let im4 = detectFaces(image: im2)
         
         detectLandmarks(image: im1)
-
-//        if selected_index == 0 {
-//            imageView.image = im3
-//        } else if selected_index == 1 {
-//            imageView.image = im4
-//        }
-//
-//        im1 = im3
-//        im2 = im4
+        
+        //        if selected_index == 0 {
+        //            imageView.image = im3
+        //        } else if selected_index == 1 {
+        //            imageView.image = im4
+        //        }
+        //
+        //        im1 = im3
+        //        im2 = im4
     }
     
     func detectLandmarks(image: UIImage) -> Void {
         
         var orientation:Int32 = 0
         
-           // detect image orientation, we need it to be accurate for the face detection to work
-           switch image.imageOrientation {
-           case .up:
-               orientation = 1
-           case .right:
-               orientation = 6
-           case .down:
-               orientation = 3
-           case .left:
-               orientation = 8
-           default:
-               orientation = 1
-           }
+        // detect image orientation, we need it to be accurate for the face detection to work
+        switch image.imageOrientation {
+        case .up:
+            orientation = 1
+        case .right:
+            orientation = 6
+        case .down:
+            orientation = 3
+        case .left:
+            orientation = 8
+        default:
+            orientation = 1
+        }
         
         let faceLandmarksRequest = VNDetectFaceLandmarksRequest(completionHandler: self.handleFaceFeatures)
         let requestHandler = VNImageRequestHandler(cgImage: image.cgImage!, orientation: CGImagePropertyOrientation(rawValue: CGImagePropertyOrientation.RawValue(orientation))!, options: [:])
@@ -103,9 +103,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         guard let observations = request.results as? [VNFaceObservation] else {
             fatalError("unexpected result type!")
         }
-
+        
         for face in observations {
             addFaceLandmarksToImage(face)
+            let lmrks = getLandmarksForFace(image: im1, face)
+            print(lmrks.count)
         }
     }
     
@@ -191,202 +193,89 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     
-    func addFaceLandmarksToImage(_ face: VNFaceObservation) {
-        var image = im1!
+    func getLandmarksForFace(image: UIImage, _ face: VNFaceObservation) -> NSMutableArray {
+        
+        let lmrks: NSMutableArray = [CGPoint()]
         
         UIGraphicsBeginImageContextWithOptions(image.size, true, 0.0)
         let context = UIGraphicsGetCurrentContext()
         
-        
-     
         // draw the image
         image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
-     
         context?.translateBy(x: 0, y: image.size.height)
         context?.scaleBy(x: 1.0, y: -1.0)
-     
+        
+        // draw the face rect
+        let w = face.boundingBox.size.width * image.size.width
+        let h = face.boundingBox.size.height * image.size.height
+        let x = face.boundingBox.origin.x * image.size.width
+        let y = face.boundingBox.origin.y * image.size.height
+        
+        
+        if let landmarks = face.landmarks?.allPoints {
+            for i in 0...landmarks.pointCount - 1 {
+                let point = landmarks.normalizedPoints[i]
+                
+                let xx = x + CGFloat(point.x) * w
+                let yy = y + CGFloat(point.y) * h
+                lmrks.add(CGPoint(x: xx, y: yy))
+            }
+        }
+        
+        return lmrks
+    }
+    
+    
+    func addFaceLandmarksToImage(_ face: VNFaceObservation) {
+        let image = im1!
+        
+        UIGraphicsBeginImageContextWithOptions(image.size, true, 0.0)
+        let context = UIGraphicsGetCurrentContext()
+        
+        // draw the image
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        
+        context?.translateBy(x: 0, y: image.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
         // draw the face rect
         let w = face.boundingBox.size.width * image.size.width
         let h = face.boundingBox.size.height * image.size.height
         let x = face.boundingBox.origin.x * image.size.width
         let y = face.boundingBox.origin.y * image.size.height
         let faceRect = CGRect(x: x, y: y, width: w, height: h)
+        
         context?.saveGState()
         context?.setStrokeColor(UIColor.red.cgColor)
         context?.setLineWidth(8.0)
         context?.addRect(faceRect)
         context?.drawPath(using: .stroke)
         context?.restoreGState()
-     
-        // face contour
+        
         context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.faceContour {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
+        context?.setStrokeColor(UIColor.green.cgColor)
+        
+        // Plot all points
+        if let landmarks = face.landmarks?.allPoints {
+            for i in 0...landmarks.pointCount - 1 {
+                let point = landmarks.normalizedPoints[i]
+                
+                context?.addEllipse(in: CGRect(
+                    origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h),
+                    size:CGSize(width: 5, height: 5)))
             }
         }
-        context?.setLineWidth(8.0)
+        
+        context?.setLineWidth(5.0)
         context?.drawPath(using: .stroke)
         context?.saveGState()
-     
-        // outer lips
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.outerLips {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.closePath()
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
-        // inner lips
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.innerLips {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.closePath()
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
-        // left eye
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.leftEye {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.closePath()
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
-        // right eye
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.rightEye {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.closePath()
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
-        // left pupil
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.leftPupil {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.closePath()
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
-        // right pupil
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.rightPupil {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.closePath()
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
-        // left eyebrow
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.leftEyebrow {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
-        // right eyebrow
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.rightEyebrow {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
-        // nose
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.nose {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.closePath()
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
-        // nose crest
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.noseCrest {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
-        // median line
-        context?.saveGState()
-        context?.setStrokeColor(UIColor.yellow.cgColor)
-        if let landmark = face.landmarks?.medianLine {
-            for i in 0...landmark.pointCount - 1 { // last point is 0,0
-                let point = landmark.normalizedPoints[i]
-                context?.addEllipse(in: CGRect(origin: CGPoint(x: x + CGFloat(point.x) * w, y: y + CGFloat(point.y) * h), size: CGSize(width: 5, height: 5)))
-            }
-        }
-        context?.setLineWidth(8.0)
-        context?.drawPath(using: .stroke)
-        context?.saveGState()
-     
+        
         // get the final image
         let finalImage = UIGraphicsGetImageFromCurrentImageContext()
-     
+        
         // end drawing context
         UIGraphicsEndImageContext()
-     
+        
         imageView.image = finalImage
     }
 }
